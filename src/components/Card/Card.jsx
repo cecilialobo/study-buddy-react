c56a79e77b416e5f4aa32f3b623b36efc89fd932
Cecilia Lobo <cecilialobo92@gmail.com>
2021-04-15 11:56:14 +0200

    Add delete button to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,20 +6,27 @@ const Card = (props) => {
 
     const [status, setStatus] = useState('to do');
     const [btnStatus, setBtnStatus] = useState('done');
+    const [btnStyle, setBtnStyle] = useState('cardContent');
 
     const checkIfDone = () => {
         status === 'to do' ? setStatus('done') : setStatus('to do');
         btnStatus === 'done' ? setBtnStatus('to do') : setBtnStatus('done');
+        btnStyle === 'cardContent' ? setBtnStyle ('cardContentDone') : setBtnStyle ('cardContent');
     }
 
     return (
         <div className='card'>
-            <h3 className='cardContent'>{props.taskTitle}</h3>
+            <h3 className={btnStyle}>{props.taskTitle}</h3>
             <p className='cardStatus cardContent'>Status: {status}</p>
             <SmallButton
                 className='cardButton'
                 onClick={checkIfDone}
                 title={`Mark as ${btnStatus}`}
+            />
+            <SmallButton
+                className='cardButton'
+                onClick={console.log('delete')}
+                title='Delete'
             />
         </div>
     )
